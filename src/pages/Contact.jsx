db722a335e772222ab30e87c7cd55b5b72845750
T-dev98 <133159152+T-dev98@users.noreply.github.com>
2024-02-03 12:44:25 -0500

    initial commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Contacts(){
+    return(
+        <div>
+            <h1>Contact me on my Email</h1>
+        </div>
+    )
+}
---
+++ src/pages/Education.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Education(){
+    return(
+        <div>
+            <h1>I am Graduated in B.A</h1>
+        </div>
+    )
+}
