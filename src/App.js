ef8efc69e8f606bab1540079dae28904920f0e0a
T-dev98 <133159152+T-dev98@users.noreply.github.com>
2024-02-03 12:43:05 -0500

    BrowserRouter logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Education from './pages/Education';
+import Projects from './pages/Projects';
+import Work from './pages/Work';
+import Signup from './pages/Signup';
+import { Route,  BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './NavlinkComponent/Navbar';
 
+//Routing using browser Router
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/work' element={<Work/>} />
+          <Route path='/projects' element={<Projects/>} />
+          <Route path='/education' element={<Education/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/Signup' element={<Signup/>} />
+          
+        </Routes>
+      </Router>
     </div>
   );
 }
