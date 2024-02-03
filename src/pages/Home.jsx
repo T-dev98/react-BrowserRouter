9e85d7e994f820b8ff214499979090003a6d2ba7
T-dev98 <133159152+T-dev98@users.noreply.github.com>
2024-02-03 14:07:05 -0500

    intial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home(){
     return(
         <div>
             <h1>Hi, Welcome to my profile</h1>
+            <Link to={"/about"}>Go to About</Link>
+            
         </div>
     )
 }
