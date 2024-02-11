89df45fbc1e4ee12cebd9b69edd1cbcc7a21a635
T-dev98 <133159152+T-dev98@users.noreply.github.com>
2024-02-11 01:27:33 -0500

    error page

---
+++ src/BrowserRouter/pages/error.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+
+export default function Error(){
+    return(
+        <div>
+            <h2>404</h2>
+            <p>Page not found</p>   
+            <Link to="/">Back to Home</Link> 
+        </div>
+    )
+}
